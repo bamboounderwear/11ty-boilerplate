37b5afccaa5650e4fc332d63fc41651ea70db0b6
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2025-03-14 13:00:50 -0400

    css

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function(eleventyConfig) {
+  // Copy the css, js, and images folders to the output folder
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
+
   return {
     dir: {
       input: "src",
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// src/js/main.js
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("JavaScript is successfully loaded!");
+  });
+  
