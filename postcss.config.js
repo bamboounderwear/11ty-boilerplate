60a48c808f8519e8d1aea15e250fb14b9fc08333
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2025-03-14 22:01:38 -0400

    delete

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,5 @@
 // postcss.config.js
 module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    process.env.NODE_ENV === 'production'
-      ? require('@fullhuman/postcss-purgecss')({
-          content: ['./dist/**/*.html'],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        })
-      : null
-  ].filter(Boolean)
-};
+    plugins: [require('autoprefixer')]
+  };
+  
