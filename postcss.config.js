81d0d29da5674fbc8bc402d251984084c60c3b92
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2025-03-14 23:58:42 -0400

    tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 // postcss.config.js
 module.exports = {
-    plugins: [require('autoprefixer')]
-  };
-  
+  plugins: [
+    require('tailwindcss'),
+    require('postcss-nested'),
+    require('autoprefixer')
+  ]
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./src/**/*.{html,njk,js}"],
+    theme: {
+      extend: {}
+    },
+    plugins: []
+  };
+  
