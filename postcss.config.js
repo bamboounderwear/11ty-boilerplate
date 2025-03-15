476071d960efabba9e46ee6b5bc9bfd5dbcd5faa
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2025-03-14 21:59:07 -0400

    css

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 // postcss.config.js
 module.exports = {
-    plugins: [require('autoprefixer')]
-  };
-  
+  plugins: [
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production'
+      ? require('@fullhuman/postcss-purgecss')({
+          content: ['./dist/**/*.html'],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        })
+      : null
+  ].filter(Boolean)
+};
