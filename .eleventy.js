aa70196315a749161f24a323856370d5ed8a83a0
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2025-03-17 11:54:16 -0400

    eleventy navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,21 @@
+const eleventyNavigationPlugin = require("eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
-    // Passthrough copy for CSS and assets directories
-    eleventyConfig.addPassthroughCopy("src/css");
-    eleventyConfig.addPassthroughCopy("src/assets");
-  
-    return {
-      dir: {
-        input: "src",
-        includes: "_includes",
-        output: "_site"
-      },
-      templateFormats: ["njk", "html", "md"],
-      htmlTemplateEngine: "njk",
-      markdownTemplateEngine: "njk"
-    };
+  // Copy CSS and assets directories to the output
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  // Add Eleventy Navigation plugin
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site"
+    },
+    templateFormats: ["njk", "html", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
   };
-  
+};
