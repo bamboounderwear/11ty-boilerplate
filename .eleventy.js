bf8598faae4535e263543fc451cc392715715756
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2025-03-17 12:56:47 -0400

    removed pagination

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,19 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-
 module.exports = function(eleventyConfig) {
   // Copy CSS and assets directories to the output folder
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/assets");
 
-  // Add the Eleventy Navigation plugin
+  const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  // Create a "blog" collection from all files in /src/blog
+  // Create a "blog" collection from files in /src/blog/posts/
   eleventyConfig.addCollection("blog", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/blog/**/*.njk");
+    return collectionApi.getFilteredByGlob("src/blog/posts/**/*.njk");
   });
 
-  // Create a "projects" collection from all files in /src/projects
+  // Create a "projects" collection from files in /src/projects/items/
   eleventyConfig.addCollection("projects", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/projects/**/*.njk");
+    return collectionApi.getFilteredByGlob("src/projects/items/**/*.njk");
   });
 
   return {
